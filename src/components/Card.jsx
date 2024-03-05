2e5b3c12da012b0ef662d611e61723fef9f93da3
AndreaHerrera20034 <106893451+AndreaHerrera20034@users.noreply.github.com>
2024-03-05 11:12:39 -0600

    Imagen corregida

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Card = () => {
     return (
         <div className="flex justify-center items-center h-screen">
-            <div className="container rounded-3xl mx-auto md:px-12 lg:py-2 lg:px-7 shadow-lg rounded-2xl overflow-hidden" style={{ backgroundColor: '#F9F7F3' }}>
+            <div className="container mx-auto md:px-12 lg:py-0 lg:px-0 rounded-3xl" style={{ backgroundColor: '#F9F7F3' }}>
                 <div className="flex flex-col lg:flex-row items-start">
                     <div className="p-6 lg:w-1/2 lg:px-8 lg:py-12">
                         <h2 className="text-2xl font-bold mb-4">Discover the art of your choice!</h2>
@@ -11,8 +11,8 @@ const Card = () => {
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, odio veritatis omnis deserunt nihil reprehenderit illum sapiente ea rerum, atque reiciendis iste repellat labore alias, voluptatem vero impedit enim laboriosam?
                         </p>
                     </div>
-                    <div className="lg:w-70 lg:h-84 relative">
-                        <img src="src\images\cardimage.jpg" className="w-full h-full object-cover object-top lg:rounded-tl-lg lg:rounded-2xl" alt="Card" />
+                    <div className="lg:w-1/2 lg:h-auto relative">
+                        <img src="src\images\cardimage.jpg" className="w-full h-full object-cover object-top md:rounded-s-none md:rounded-2xl" alt="Card" />
                     </div>
                 </div>
             </div>
