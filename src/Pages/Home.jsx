881b389be7ec1f633305daeaae24e224df44df88
Alondra Chable Carrillo <106031585+AloloCarr@users.noreply.github.com>
2024-03-05 13:45:30 -0600

    actualizacion carrito y ventas

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "../components/navbar";
 
 const Home = () => {
   return <>
-
+    <Header></Header>
     <h1 class="test-xl flex items-center justify-center">Home</h1>
+    
   </>
 
 }
